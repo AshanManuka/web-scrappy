62e7a93975444b4b22da683197afa9e170a29a89
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-11-29 11:55:28 +0530

    finding job title and company in topjobs.com

--- scrapy.py
+++ scrapy.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get("https://www.daraz.lk/mens-watches/").text
+page = requests.get('https://www.topjobs.lk/applicant/vacancybyfunctionalarea.jsp;jsessionid=KS5djcqsnRSHv9yv-g9E8X9w?FA=SDQ').text
 
 soup = BeautifulSoup(page, 'lxml')
 
-desc_box = soup.find_all('div', class_='box--ujueT')
-print(desc_box)
+desc_box = soup.find_all('table', class_='tbldata_2 vbfa-table')
 
 for p in desc_box:
-    item = desc_box.find('div', class_='gridItem--Yd0sa')
-    print(item)
+    job = p.find_all('td')
+
+    for i in job:
+        title = i.find('h2')
+        company = i.find('h1')
+        if title is not None and company is not None:
+            print("Job Title : ",title.text)
+            print("Company : ",company.text)
+            print(" ")
+            print("----------------------------------------------------------------")
+            
